9ab48fda687118f156d6a5e772ddaa0f1b1180a3
SouriCat <51195435+SouriCat@users.noreply.github.com>
2019-06-06 14:29:06 +0200

    Add files via upload

---
+++ 3_Pratique__ Tableaux.py
@@ -0,0 +1,31 @@
+"Viserion","Drogon","Rhaegal","Rhaegal","Drogon","Viserion","Drogon","Rhaegal"
+
+if <> == "Viserion" :
+    print ("La valeur est bien égale a Viserion")
+if <> == "Drogon" :
+    print ("La valeur est bien égale a Drogon")
+if <> == "Rhaegal" :
+    print ("La valeur est bien égale a Rhaegal")
+
+
+
+
+0,1,2,2,4,1,2,1,3,5
+1,3,0,2,1,4,1,3,5,1
+4,5,2,3,5,1,3,4,1,2
+1,3,5,1,3,0,2,1,4,0
+
+
+if <> == 0 :
+    print ("La valeur est bien égale a 0")
+if <> == 2 :
+    print ("La valeur est bien égale a 2")
+if <> == 2 :
+    print ("La valeur est bien égale a 2")
+if <> == 1 :
+    print ("La valeur est bien égale a 1")
+if <> == 5 :
+    print ("La valeur est bien égale a 5")
+if <> == 4 :
+    print ("La valeur est bien égale a 4")
+    
